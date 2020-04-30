44b5c607af9fb7f6a2d61cd66eb9f6115a30c0a2
Uday Teja Ganesuni <udayteja.g@Technovert.com>
2020-04-30 20:58:13 +0530

    Added Chrome Tabs Functionality

---
+++ TabFinder/ChromeData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabFinder
+{
+    public class ChromeData
+    {
+        const int NumTopTabs = 10;
+
+        List<int> Counts = new List<int>();
+        public ArrayList AllTabsUsed = new ArrayList();
+        int IndexProcesChrome = -1;
+        Process[] List;
+
+        public int Delay = 250;
+        public bool IsActive { get; private set; }
+
+        public string CurrentTab = "";
+
+        public string[] TopUsedTabs = new string[NumTopTabs];
+
+
+        public bool IsChromeOpened()
+        {
+            List = Process.GetProcessesByName("chrome");
+            if (List.Count() == 0)
+                return false;
+            else
+                return true;
+        }
+
+        public void StartMonitoring()
+        {
+            IsActive = true;
+            while (IsActive)
+            {
+                if (!IsChromeOpened())
+                {
+                    StopMonitoring();
+                    break;
+                }
+                else
+                {
+                    IndexProcesChrome = -1;
+                    foreach (Process p in List)
+                    {
+                        IndexProcesChrome += 1;
+                        if (p.MainWindowTitle != "")
+                            break;
+                    }
+                    Process Proc = List[IndexProcesChrome];
+                    try
+                    {
+                        CurrentTab = Proc.MainWindowTitle.Remove(Proc.MainWindowTitle.Length - 15);
+                        if (!AllTabsUsed.Contains(Proc.MainWindowTitle))
+                        {
+                            AllTabsUsed.Add(Proc.MainWindowTitle);
+                            Counts.Add(1);
+                        }
+                        else
+                            Counts[AllTabsUsed.IndexOf(Proc.MainWindowTitle)] += 1;
+                        OrderMostUsedTabs();
+                    }
+                    catch (Exception e) { };
+                }
+            }
+        }
+
+        private void OrderMostUsedTabs()
+        {
+            int[] TempCounts = Counts.ToArray();
+            for (int i = 0; (i < NumTopTabs) && (i < Counts.Count); i++)
+            {
+                int Max = 0;
+                for (int j = 0; j < Counts.Count; j++)
+                    if ((TempCounts[j] > TempCounts[Max]))
+                        Max = j;
+                string str = AllTabsUsed[Max].ToString();
+                TopUsedTabs[i] = str.Remove(str.Length - 15);
+                TempCounts[Max] = -1;
+            }
+        }
+
+        public void StopMonitoring()
+        {
+            IsActive = false;
+            CurrentTab = "";
+        }
+    }
+}
--- TabFinder/Program.cs
+++ TabFinder/Program.cs
@@ -1,79 +1,36 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Threading;
-using UIAutomationClient;
 
 namespace TabFinder
 {
     class Program
     {
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetForegroundWindow();
+        public static ChromeData chromeData;
 
-        static bool TryGetMSEdgeUrlAndTitle(IntPtr edgeWindow, out string url, out string title)
+        public static void Main(string[] args)
         {
-            const int UIA_NamePropertyId = 30005;
-            const int UIA_ClassNamePropertyId = 30012;
-            const int UIA_NativeWindowHandlePropertyId = 30020;
-
-            url = "";
-            title = "";
-
-            IUIAutomation uiA = new CUIAutomation();
-            IUIAutomationElement rootElement = uiA.GetRootElement();
-
-            IUIAutomationCacheRequest cacheRequest = uiA.CreateCacheRequest();
-            cacheRequest.AddProperty(UIA_NamePropertyId);
-
-            IUIAutomationCondition windowCondition = uiA.CreatePropertyCondition(UIA_NativeWindowHandlePropertyId, GetForegroundWindow());
-            IUIAutomationElement windowElement = rootElement.FindFirstBuildCache(TreeScope.TreeScope_Descendants, windowCondition, cacheRequest);
-            if (windowElement == null)
-                return false;
-
-            IUIAutomationCondition edgeCondition = uiA.CreatePropertyCondition(UIA_NamePropertyId, "Microsoft Edge");
-            IUIAutomationElement edgeElement = windowElement.FindFirstBuildCache(TreeScope.TreeScope_Subtree, edgeCondition, cacheRequest);
-            if (edgeElement == null)
-                return false;
-
-            IUIAutomationCondition tabCondition = uiA.CreatePropertyCondition(UIA_ClassNamePropertyId, "TabWindowClass");
-            IUIAutomationElement tabElement = edgeElement.FindFirstBuildCache(TreeScope.TreeScope_Descendants, tabCondition, cacheRequest);
-            if (tabElement == null)
-                return false;
-
-            IUIAutomationCondition ieCondition = uiA.CreatePropertyCondition(UIA_ClassNamePropertyId, "Internet Explorer_Server");
-            IUIAutomationElement ieElement = tabElement.FindFirstBuildCache(TreeScope.TreeScope_Descendants, ieCondition, cacheRequest);
-            if (ieElement == null)
-                return false;
-
-            url = ieElement.CachedName;
-            title = tabElement.CachedName;
-
-            return true;
+            chromeData = new ChromeData();
+            Timer t = new Timer(TimerCallback, null, 0, 3000);
+            if (chromeData.IsChromeOpened())
+            {
+                chromeData.StartMonitoring();
+            }
         }
 
-        static void Main(string[] args)
+        private static void TimerCallback(object obj)
         {
-            string oldUrl = "";
-            string oldTitle = "";
-
-            while (true)
-            {
-                string url = "";
-                string title = "";
-
-                if (TryGetMSEdgeUrlAndTitle(GetForegroundWindow(), out url, out title))
-                {
-                    if ((url != oldUrl) || (title != oldTitle))
-                    {
-                        Console.WriteLine(String.Format("Page title: {0} \r\nURL: {1}", title, url));
-
-                        oldUrl = url;
-                        oldTitle = title;
-                    }
-                }
-
-                Thread.Sleep(250);
-            }
+            Console.WriteLine($"Active Tab at {DateTime.Now} is {chromeData.CurrentTab}");
+            //Console.WriteLine("Top Used Tabs");
+            //foreach (var item in chromeData.TopUsedTabs)
+            //{
+            //    Console.WriteLine($"{item}");
+            //}
+            //Console.WriteLine("Top Used Tabs");
+            //foreach (var item in chromeData.AllTabsUsed)
+            //{
+            //    Console.WriteLine($"{item}");
+            //}
+            //Console.WriteLine("----------------------------");
         }
     }
 }
